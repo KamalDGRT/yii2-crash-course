b959cb13e3ad18e549d56a1e4c9bbd820e25a7fa
Kamal Sharma <kamaldgrt@gmail.com>
2020-06-17 13:34:01 +0530

    Update view.php

--- views/article/view.php
+++ views/article/view.php
@@ -10,32 +10,34 @@ $this->title = $model->title;
 $this->params['breadcrumbs'][] = ['label' => 'Articles', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
+
 ?>
 <div class="article-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    Created at: <?php echo Yii::$app->formatter->asRelativeTime($model->created_at) ?>
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <?php if (!Yii::$app->user->isGuest && Yii::$app->user->id === $model->created_by): ?>
+        <p>
+            <?php echo Html::a('Update', ['update', 'slug' => $model->slug], ['class' => 'btn btn-primary']) ?>
+            <?php echo Html::a('Delete', ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger',
+                'data' => [
+                    'confirm' => 'Are you sure you want to delete this item?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+        </p>
+    <?php endif ?>
 
     <div>
-        <!---<h1><?php //echo Html::encode($model->title) ?></h1> -->
+        <h1><?php echo Html::encode($model->title) ?></h1>
         <p class="text-muted">
             <small>
+                Created at: <?php echo Yii::$app->formatter->asRelativeTime($model->created_at) ?>
                 By: <?php echo $model->createdBy->username ?>
             </small>
         </p>
         <div>
-            <?php echo $model->getEncodedBody();  ?>
+            <?php echo Html::encode($model->body); ?>
         </div>
     </div>
+
 </div>
